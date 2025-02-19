3354bcfb3111e6c1038a97d26b100097372cbf5f
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-02-19 21:36:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ async function trialDivisionFromFile(number) {
     let factors = [];
     let lastLoggedPrime = 0n;
     let limit = number >= 10n ** 10n ? 100000 : primes.length; // 10^10 以上なら 10万まで試す
-    limit = Math.min(limit, 499979, primes.length); // primes.length を超えないようにする
+    limit = Math.max(limit, 499979 : primes.length); // primes.length を超えないようにする
     
     try {
         for (let i = 0; i < limit; i++) {
