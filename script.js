8f00d55361d29c1981c691b419ddff64a1566c36
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-02-20 19:38:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -212,13 +212,7 @@ async function pollardsRhoFactorization(number) {
             break;
         }
 
-        let factor = null;
-        if (number >= 10n ** 17n) {
-            factor = await ecmFactorization(number); // `await` を追加
-        }
-        if (!factor) {
-            factor = pollardsRho(number);
-        }
+        let factor = pollardsRho(number);
 
         if (!factor || factor === number) {
             factors.push(number);
