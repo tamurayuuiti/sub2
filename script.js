eb4cd52a98d1cd3221de8aa211dbf7a2d521f287
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-02-23 15:29:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ async function ecmFactorization(n) {
     // 事前に素数判定し、素数ならすぐ返す
     if (isPrimeMillerRabin(n)) {
         console.log(`  初期チェック: ${n} は素数`);
-        return factors; // ここで素数なら即終了
+        return [n]; // ここで素数なら即終了
     }
     
     // 最大公約数を求める関数
