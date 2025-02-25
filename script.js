df8fb464527295574858bf67850b8cd4b78b892d
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-02-25 16:07:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -331,8 +331,8 @@ async function ecmFactorization(n) {
         return x0;
     }
 
-    const maxCurves = n > 10n ** 20n ? 100 : 50;
-    const B1 = 500000n, B2 = 1500000n;
+    const maxCurves = n > 10n ** 20n ? 1000 : 500;
+    const B1 = 5000000n, B2 = 15000000n;
 
     for (let i = 0; i < maxCurves; i++) {
         const a = BigInt(Math.floor(Math.random() * Number(n)));
