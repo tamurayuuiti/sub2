8d479b452daa96815e8cb027553f6a1cf1ab13d8
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-02-25 13:53:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -344,7 +344,7 @@ async function ecmFactorization(n) {
 
         let factor = gcd(2n * y, n);
         if (factor > 1n && factor < n) {
-            return await processFactor(factor);
+            return await processFactor(factor, remainder);
         }
 
         let k = 2n;
@@ -354,7 +354,7 @@ async function ecmFactorization(n) {
             k *= 2n;
             factor = gcd(x - y, n);
             if (factor > 1n && factor < n) {
-                return await processFactor(factor);
+                return await processFactor(factor, remainder);
             }
         }
 
@@ -366,7 +366,7 @@ async function ecmFactorization(n) {
                 let yj = modmul(y, modInverse(j + 1n, n), n);
                 let factor = gcd(xj - yj, n);
                 if (factor > 1n && factor < n) {
-                    return processFactor(factor);
+                    return processFactor(factor, remainder);
                 }
             })());
         }
