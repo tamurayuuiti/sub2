f90d03230db577ccfa4650f398238f6393147f16
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-02-28 19:19:01 +0900

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -233,7 +233,7 @@ async function pollardsRho(n) {
 
         x = ys;
         if (d === 1n) {
-            m *= 2n;
+            m = (m * 3n) / 2n;
             if (m > 10n ** 6n) {
                 throw new Error("エラー: m が異常に大きくなっています。計算を停止します。");
             }
