b0d98a71377c2a2f17023d6a2c5c8cbe579f849a
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-03-07 11:20:16 +0900

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -225,7 +225,7 @@ async function pollardsRho(n) {
                 if (d > 1n) break;
             }
 
-            if (i % 10000n === 0n) {
+            if (i % 5000n === 0n) {
                 await new Promise(resolve => setTimeout(resolve, 0));  
             }
         }
