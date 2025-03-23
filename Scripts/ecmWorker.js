5b80110720e182afb3996d4ac04c22b00607d82c
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-03-23 21:17:54 +0900

    Update ecmWorker.js

--- Scripts/ecmWorker.js
+++ Scripts/ecmWorker.js
@@ -1,7 +1,5 @@
 importScripts("ecmUtils.js");
 
-console.log("🚀 Web Worker ecmWorker.js が開始されました！");
-
 self.onmessage = async function(event) {
     const number = BigInt(event.data);
     console.log(`🚀 Worker: ECM を実行 (${number})`);
@@ -11,11 +9,9 @@ self.onmessage = async function(event) {
     try {
         const factor = await ecm(number, msg => self.postMessage({ type: "log", message: msg }));
 
-        console.log(`✅ Worker: ECM 完了 (${factor})`);
         self.postMessage({ type: "log", message: `✅ Worker: ECM 終了 (${factor})` });
         self.postMessage({ type: "result", factor: factor ? factor.toString() : "null" });
     } catch (error) {
-        console.error(`❌ Worker: ECM エラー - ${error.message}`);
         self.postMessage({ type: "log", message: `❌ Worker: エラー発生 - ${error.message}` });
         self.postMessage({ type: "result", factor: "null" });
     }
