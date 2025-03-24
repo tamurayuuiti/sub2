4a14775f20300bca0fd0243cbf5abb69db71390b
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-03-24 18:46:59 +0900

    Update pollardsRho.js

--- Scripts/pollardsRho.js
+++ Scripts/pollardsRho.js
@@ -135,7 +135,7 @@ export function getDigitBasedParams(n, attempt = 0) {
         } else if (attempt === 1) {
             fxFunction = (x, c, n) => ((x * x + c * x) % n);
             fxFunctionString = "(x² + c x) % n";
-            MAX_TRIALS = 30000000;
+            MAX_TRIALS = 3000000;
         } else if (attempt === 2) {
             fxFunction = (x, c, n) => ((x * x * x + 3n * x + c) % n);
             fxFunctionString = "(x³ + 3x + c) % n";
