8bac91c92f98da530fcf8620b3d066c0fa063d31
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-03-27 00:11:55 +0900

    Update Worker.js

--- Scripts/Worker.js
+++ Scripts/Worker.js
@@ -13,7 +13,7 @@ self.onmessage = async function(event) {
         };
 
         if (fxType === "fx1") {
-            fxEquation = "(x² + 7x + c) mod n";
+            fxEquation = "(x² + 7x + c) / n";
             fxFunction = (x, c, n) => (x * x + 7n * x + c) % n;
         } else if (fxType === "fx2") {
             fxEquation = "(x² + cx) mod n";
@@ -25,7 +25,7 @@ self.onmessage = async function(event) {
             throw new Error("Unknown fxType");
         }
 
-        console.log(`worker${workerId + 1} の実行成功: fx = ${fxEquation} (試行上限 ${MAX_TRIALS[fxType]}), c = ${c} (範囲: 1 ～ ${maxC * 2 - 1})`);
+        console.log(`worker${workerId + 1} の実行成功: fx = ${fxEquation}, 試行上限 ${MAX_TRIALS[fxType]}, c = ${c} (範囲: 1 ～ ${maxC * 2 - 1})`);
 
         let x = 2n, y = 2n, d = 1n;
         let trialCount = 0n;
@@ -45,13 +45,13 @@ self.onmessage = async function(event) {
                 trialCount++;
 
                 if (q === 0n) {
-                    console.error(`worker${workerId + 1} [fx = ${fxEquation}] q が 0 になりました。（リセット回数: ${resetCount}）`);
+                    console.error(`worker${workerId + 1} (${fxEquation}) q が 0 になりました。（リセット回数: ${resetCount}）`);
                     q = 1n;
                     resetCount++;
                 }
 
                 if (trialCount % 2500000n === 0n) {
-                    console.log(`worker${workerId + 1} [fx = ${fxEquation}] 試行 ${trialCount},　fx = ${fxType}, x=${x}, y=${y}, q=${q}, gcd=${d}`);
+                    console.log(`worker${workerId + 1} (${fxEquation}) 試行 ${trialCount},　fx = ${fxType}, x=${x}, y=${y}, q=${q}, gcd=${d}`);
                     await new Promise(resolve => setTimeout(resolve, 0));
                 }
 
@@ -67,7 +67,7 @@ self.onmessage = async function(event) {
         }
 
         if (d > 1n && d !== n) {
-            console.log(`worker${workerId + 1} [fx = ${fxEquation}] が因数 ${d} を送信（試行回数: ${trialCount}）`);
+            console.log(`worker${workerId + 1} (${fxEquation}) が因数 ${d} を送信（試行回数: ${trialCount}）`);
             
             setTimeout(() => {
                 postMessage({ factor: d.toString(), trials: trialCount.toString() });
@@ -76,7 +76,7 @@ self.onmessage = async function(event) {
             return;
         }
 
-        console.log(`worker${workerId + 1} [fx = ${fxEquation}] が試行上限 ${MAX_TRIALS[fxType]} に達したため停止。`);
+        console.log(`worker${workerId + 1} (${fxEquation}) が試行上限 ${MAX_TRIALS[fxType]} に達したため停止。`);
         postMessage({ stopped: true });
 
     } catch (error) {
