a443244f0caa796353a481b219986ceb89425bd4
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-03-26 21:01:48 +0900

    Update Worker.js

--- Scripts/Worker.js
+++ Scripts/Worker.js
@@ -1,8 +1,8 @@
-console.log("ロード成功: 利用可能なスレッド数: ${navigator.hardwareConcurrency}, fxType = ${fxType}");
-
 self.onmessage = async function(event) {
     try {
         let { n, fxType } = event.data;
+
+        console.log("ロード成功: 利用可能なスレッド数: ${navigator.hardwareConcurrency}, fxType = ${fxType}");
 
         const MAX_TRIALS = {
             fx1: 500000n,
