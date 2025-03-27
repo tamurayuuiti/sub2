c0388c34f4e07f46d33fde45a68ec369a2b443ef
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-03-27 10:46:57 +0900

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -1,6 +1,9 @@
 // ミラー・ラビン素数判定法
 import { isPrimeMillerRabin } from './Scripts/millerRabin.js';
 
+// 試し割り法
+import { trialDivision } from './Scripts/trialDivision.js';
+
 // Pollard’s rho 法
 import { pollardsRhoFactorization } from './Scripts/pollardsRho.js';
 
@@ -135,38 +138,6 @@ async function startFactorization() {
         document.getElementById("spinner").style.display = "none";
         document.getElementById("loading").style.display = "none";
     }
-}
-
-// 外部ファイルを使った試し割り法
-async function trialDivision(number) {
-    let factors = [];
-
-    // 最大素数の設定
-    const MAX_PRIME = number >= 10n ** 10n ? 100000n : 499979n;
-
-    try {
-        for (let i = 0; i < primes.length; i++) {
-            if (primes[i] === undefined) break;
-            let prime = BigInt(primes[i]);
-            if (prime > MAX_PRIME) break;
-            if (prime * prime > number) break;
-
-            while (number % prime === 0n) {
-                factors.push(prime);
-                number /= prime;
-            }
-
-            if (i % 5000 === 0) {
-                console.log(`現在の素数: ${prime}`);
-                await new Promise(resolve => setTimeout(resolve, 0));
-            }
-        }
-
-    } catch (error) {
-        console.error("試し割りエラー:", error);
-        document.getElementById("result").textContent = "試し割り中にエラーが発生しました";
-    }
-    return { factors, remainder: number };
 }
 
 loadPrimes();
