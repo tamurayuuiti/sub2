bb04b505c27d4a803aa43e699321fcf167478d99
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-04-02 23:32:24 +0900

    Update worker.js

--- Scripts/worker.js
+++ Scripts/worker.js
@@ -82,7 +82,7 @@ self.onmessage = async function(event) {
             return false;
         }
 
-        while (cRetryCount < MAX_C_RETRIES[fxType]) {
+        while (cRetryCount < MAX_C_RETRIES) {
             let c = getRandomC(n, maxC);
             let success = await runFactorization(c);
             if (success) return;
