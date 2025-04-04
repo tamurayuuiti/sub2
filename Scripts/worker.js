b6dc847c2d98f93a3a834e1521ae300b4f208c84
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-04-04 23:46:53 +0900

    Update worker.js

--- Scripts/worker.js
+++ Scripts/worker.js
@@ -31,7 +31,7 @@ self.onmessage = async function(event) {
             let trialCount = 0n;
             let q = 1n;
             let m = 128n;
-            let k = 8n;
+            let k = 15n;
             let resetCount = 0;
 
             x = fxFunction(x, c, n);
