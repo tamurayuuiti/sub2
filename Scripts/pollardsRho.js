2425d05a5d688d9a07b9421db4c5f09cba7fea21
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-04-05 02:30:23 +0900

    Update pollardsRho.js

--- Scripts/pollardsRho.js
+++ Scripts/pollardsRho.js
@@ -18,7 +18,7 @@ export async function pollardsRho(n) {
                 const initialX = assignX(i, n, workerCount);
                 const mMultiplier = BigInt(Math.floor(getMMultiplier(i) * 100));
 
-                worker.postMessage({ n, workerId: i, initialX, mMultiplier: mMultiplier.toString() });
+                worker.postMessage({ n, workerId: i, initialX, mMultiplier });
 
                 worker.onmessage = function (event) {
                     console.log(`受信データ:`, event.data);
