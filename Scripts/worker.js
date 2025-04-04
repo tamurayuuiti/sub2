3b270ffb83595d9e7962ff66f09f3fb1c06097b0
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-04-05 02:41:20 +0900

    Update worker.js

--- Scripts/worker.js
+++ Scripts/worker.js
@@ -8,7 +8,7 @@ self.onmessage = async function(event) {
         const fxEquation = "(x³ + 5x) mod n";
         const fxFunction = (x, n) => (x * x * x + 5n * x) % n;
 
-        console.log(`Worker ${workerId + 1} を実行: fx = ${fxEquation}, 初期 x = ${initialX}, 試行上限 ${MAX_TRIALS}`);
+        console.log(`worker ${workerId + 1} を実行: fx = ${fxEquation}, 初期 x = ${initialX}, 試行上限 ${MAX_TRIALS}`);
 
         let x = initialX, y = initialX, d = 1n;
         let trialCount = 0n;
