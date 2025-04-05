c864a441b91ebf357868f1797d2a9b511036c184
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-04-05 19:08:40 +0900

    Update pollardsRho.js

--- Scripts/pollardsRho.js
+++ Scripts/pollardsRho.js
@@ -86,6 +86,6 @@ function getRandomX(n) {
 }
 
 function getMMultiplier(workerId) {
-    const multipliers = [175n, 200n, 150n];
+    const multipliers = [250n, 200n, 150n];
     return multipliers[workerId % multipliers.length];
 }
